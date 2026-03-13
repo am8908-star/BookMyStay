fb23e5843a1a3830d7697aa8e5692aa64df998d3
am8908-star <am8908@srmist.edu.in>
2026-03-13 13:59:11 +0530

    UC1

--- src/BookMyShow.java
+++ src/BookMyShow.java
@@ -1,2 +1,17 @@
+
 public class BookMyShow {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("   Hotel Booking System v1.0     ");
+        System.out.println("=================================");
+
+        // Inform user that application has started successfully
+        System.out.println("Application started successfully.");
+
+        // Inform that the program is terminating
+        System.out.println("Thank you for using Book My Stay!");
+    }
 }
